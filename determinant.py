1c7b35b2310436f1664d997d8ee745e9fed30a42
MD AKHTAR <155041297+akhtarsru@users.noreply.github.com>
2023-12-28 10:50:54 +0000

    ecsdffdfd

--- determinant.py
+++ determinant.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.linalg import det
 
 def determinant():
-    A=np.array([[1,2,3],[4,3,6],[2,1,4]])
+    A=np.array([[0,2,3],[3,2,9],[7,1,4]])
     val=det(A)
     print(val)
 
